d3bc9aadd5a5543f672f6dd75f11db3b5bc721e7
Migg23 <arellanomiguel676@gmail.com>
2025-10-18 18:50:52 -0500

    dashboard started

---
+++ financeapp/src/components/homepages/FinanceInformation.js
@@ -0,0 +1,19 @@
+
+
+function FinanceInformation(){
+
+
+
+    return(
+        //this is the Finance information
+        <div >
+            <h3>Why create a budget ?</h3>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
+
+
+export default FinanceInformation;
