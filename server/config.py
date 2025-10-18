a11463083a4ea2fa2a390526674aa5d2e579567f
Mikey Bradley <mwbradley@ksu.edu>
2025-10-18 17:46:41 -0500

    Im sorry

--- server/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/finance_user' #finance_user is database name
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your-secret-key'
