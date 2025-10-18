663356e188e82e706e960ad8f97e32678e62da1a
theedermartinez <edermar14@gmail.com>
2025-10-18 17:24:03 -0500

    register app thingy

--- financeapp/src/App.js
+++ financeapp/src/App.js
@@ -1,23 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-import Navbar from './components/homepages/Navbar';
+
 import HomePage from './components/homepages/Homepage';
 import { lazy, Suspense } from 'react';
+import RegisterPage from './components/loginpages/RegisterPage';
 
 const LoginPage = lazy(() => import('./components/loginpages/LoginPage'));
 
 function App() {
   return (
     <Router>
-      
+    
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route
           path="/login"
           element={
-            <Suspense fallback={<div>Loading...</div>}>
+            <Suspense fallback={<div>Loading Login...</div>}>
               <LoginPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Suspense fallback={<div>Loading Register...</div>}>
+              <RegisterPage />
             </Suspense>
           }
         />
