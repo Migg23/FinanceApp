136e95ac72e91f13e32956fcb40100f9d6074cca
Mikey Bradley <mwbradley@ksu.edu>
2025-10-18 17:20:26 -0500

    Added more functions to app

---
+++ server/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from server.config import Config
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    CORS(app, resource={r"/*": {"origins": {"http://localhost:3000"}}})
+    db.init_app(app)
+
+    from server.userdata.userdata import userdata_bp
+    app.register_blueprint(userdata_bp)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
