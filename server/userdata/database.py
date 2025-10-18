4249c7d5ff7ba42df231148a988cc9c97fc96ba7
Mikey Bradley <mwbradley@ksu.edu>
2025-10-18 10:42:12 -0500

    Added id to database

--- server/userdata/database.py
+++ server/userdata/database.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), unique=False, nullable=False)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(50), unique=False, nullable=False)
